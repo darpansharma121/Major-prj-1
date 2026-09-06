9834391b2e29687ee1fa459054068c62623e3c4d
Darpan sharma <darpansharma121@gmail.com>
2026-09-06 22:23:22 +0530

    Completed the review part

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const Review = require("./review.js");
 
 const listingSchema = new Schema({
    title:{
@@ -40,4 +41,15 @@ const listingSchema = new Schema({
       },
    ],
 });
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+   if (listing) {
+      await Review.deleteMany({
+         _id: {
+            $in: listing.reviews,
+         },
+      });
+   }
+})
+
 module.exports=mongoose.model("listing",listingSchema);
