8c122aa855ed529a8502ff5398def6612728153c
Darpan sharma <darpansharma121@gmail.com>
2026-09-06 21:36:11 +0530

    Reviews section modified

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride=require("method-override")
 const ejsMate=require('ejs-mate');
 const wrapAsync = require("./utils/wrapAsync.js")
 const ExpressError= require("./utils/ExpressError.js")
-const {  listingSchema } = require("./schema.js");
+const {  listingSchema, reviewSchema} = require("./schema.js");
 const Review= require("./models/review.js");
 
 main()
@@ -35,6 +35,17 @@ app.get('/',(req,res)=>{
 
 const validateListing=(req,res,next)=>{
      let {error}= listingSchema.validate(req.body);
+ 
+  if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+};
+
+const validateReview =(req,res,next)=>{
+     let {error}= reviewSchema.validate(req.body);
  
   if(error){
     let errMsg=error.details.map((el)=>el.message).join(",");
@@ -99,16 +110,16 @@ res.redirect("/listings");
 }));
 
 //Reviews
-app.post("/listings/:id/reviews",async (req, res)=>{
+app.post("/listings/:id/reviews",validateReview, wrapAsync(async (req, res)=>{
 let listing = await Listing.findById(req.params.id);
 let newReview= new Review(req.body.review);
 
 listing.reviews.push(newReview);
 await newReview.save();
 await listing.save();
-// res.redirect(`/listings/${listing._id}`)
-res.send("Review added successfully");
-});
+res.redirect(`/listings/${listing._id}`)
+
+}));
     
 
 //  app.get('/listings', async (req, res)=>{
