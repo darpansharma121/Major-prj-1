3b170c5a4cfeb8268e21908e6774d5e097d5e9c2
Darpan sharma <darpansharma121@gmail.com>
2026-09-11 22:26:50 +0530

    Added middleware

--- routes/user.js
+++ routes/user.js
@@ -36,7 +36,15 @@ router.post("/login",passport.authenticate("local",{failureFlash:true,failureRed
     res.redirect("/listings");
 });
 
-
+router.get("/logout", (req,res,next)=>{
+    req.logout((err)=>{
+        if(err){
+           return next(err);
+        }
+        req.flash("success", "you are logged out!");
+        res.redirect("/listings")
+    })
+})
 
 
 module.exports=router;
