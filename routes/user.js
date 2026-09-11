898c01d0512f1f31ab9e20a1f2e09cdb598b109a
Darpan sharma <darpansharma121@gmail.com>
2026-09-11 11:30:29 +0530

    Added Login

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express= require("express");
 const router=express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync")
+const passport=require("passport");
+
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -22,7 +24,19 @@ router.post("/signup",wrapAsync(async (req,res)=>{
     }
 
 
-}))
+}));
+
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+});
+
+router.post("/login",passport.authenticate("local",{failureFlash:true,failureRedirect:"/login"}),(req,res)=>{
+    req.flash("success", "Welcome back!");
+    res.redirect("/listings");
+});
+
+
 
 
 module.exports=router;
