265f3050b56937caeeec74067bb8dedbfa51527d
Darpan sharma <darpansharma121@gmail.com>
2026-09-11 22:56:56 +0530

    Added extra flow on the users convinient

--- middleware.js
+++ middleware.js
@@ -1,9 +1,16 @@
  module.exports.isLoggedIn =(req,res,next)=>{
 
      if(!req.isAuthenticated()){
+        req.session.redirectUrl = req.originalurl;
         req.flash("error", "You must be signed in to create a new listing!");
         return res.redirect("/login");
     }
     next();
  }
  
+ module.exports.saveRedirectUrl =(req,res,next) =>{
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+ }
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router=express.Router();
 const User=require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync")
 const passport=require("passport");
+const { saveRedirectUrl} =require("../middleware.js");
 
 
 router.get("/signup",(req,res)=>{
@@ -15,8 +16,13 @@ router.post("/signup",wrapAsync(async (req,res)=>{
     const newUser= new User({email,username});
      const registeredUser= await User.register(newUser,password);
      console.log(registeredUser);
-     req.flash("success", "Welcome to Wanderlust");
+     req.login(registeredUser,(err)=>{
+        if(err){
+            return next(err);
+        }   req.flash("success", "Welcome to Wanderlust");
      res.redirect("/listings");
+     });
+  
     }
     catch(e){
         req.flash("error", e.message);
@@ -31,9 +37,14 @@ router.get("/login",(req,res)=>{
     res.render("users/login.ejs");
 });
 
-router.post("/login",passport.authenticate("local",{failureFlash:true,failureRedirect:"/login"}),(req,res)=>{
+router.post("/login",
+    saveRedirectUrl,
+    passport.authenticate("local",{failureFlash:true,
+    failureRedirect:"/login"}),
+    async (req,res)=>{
     req.flash("success", "Welcome back!");
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
 });
 
 router.get("/logout", (req,res,next)=>{
