4baef39fffd9d42329e77e77606fb770cebabb2e
Darpan sharma <darpansharma121@gmail.com>
2026-09-12 20:28:40 +0530

    Completed a ready to deploy app

--- routes/review.js
+++ routes/review.js
@@ -5,7 +5,7 @@ const ExpressError= require("../utils/ExpressError.js")
 const {  reviewSchema} = require("../schema.js");
 const Review= require("../models/review.js");
 const Listing = require("../models/listing.js");
-const { validateReview, isLoggedIn } = require("../middleware.js");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
 router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res)=>{
     let listing = await Listing.findById(req.params.id);
@@ -19,7 +19,7 @@ router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res)=>{
 }));
 
 // Delete reviews
-router.delete("/:reviewId", isLoggedIn, wrapAsync(async (req, res) => {
+router.delete("/:reviewId", isLoggedIn,isReviewAuthor, wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
     await Listing.findByIdAndUpdate(id, { $pull: { reviews: { _id: reviewId } } });
     await Review.findByIdAndDelete(reviewId);
